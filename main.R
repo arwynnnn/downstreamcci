848815b1d2b31b616cb2210b3e0d062f60b03269
Kacper Maciejewski <67686532+arwynnnn@users.noreply.github.com>
2025-03-27 22:46:32 +0100

    Update main.R

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 #setwd("/home/projects2/downstreamcci")
-source("/home/projects2/downstreamcci/KNN_imputation.R")
-source("/home/projects2/downstreamcci/DownstreamCCI.R")
-source("/home/projects2/downstreamcci/DownstreamCCI_utils.R")
+source("/home/projects2/kam_project/downstreamcci/KNN_imputation.R")
+source("/home/projects2/kam_project/downstreamcci/DownstreamCCI.R")
+source("/home/projects2/kam_project/downstreamcci/DownstreamCCI_utils.R")
 library(Seurat)
 library(SeuratObject)
 
